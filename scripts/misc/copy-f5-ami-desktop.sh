daf73adaeb3601f4d3aea4fb66500be633a379f6
Jason Chiu <f5jason@users.noreply.github.com>
2023-04-05 14:47:16 -0400

    update scripts

--- scripts/misc/copy-f5-ami-desktop.sh
+++ scripts/misc/copy-f5-ami-desktop.sh
@@ -1,2 +1,2 @@
 # Copy F5 BIG-IP AMI subscription link to jump host desktop
-cp ./AWS-F5-BIG-IP-AMI /mnt/c/Users/user/Desktop/"AMI-F5 Advanced WAF (PAYG, 25Mbps).url"
+cp ./aws-ami-bigip.url /mnt/c/Users/user/Desktop/"AMI-F5 Advanced WAF (PAYG, 25Mbps).url"
