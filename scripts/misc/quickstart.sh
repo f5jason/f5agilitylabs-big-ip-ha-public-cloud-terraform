6a06dadda71a68101cbbd42562d9dfb86ad8567a
Jason Chiu <f5jason@users.noreply.github.com>
2023-04-08 12:05:26 -0400

    Update quickstart.sh

--- scripts/misc/quickstart.sh
+++ scripts/misc/quickstart.sh
@@ -1,5 +1,6 @@
 # Lab quickstart script
-# Usage: 
+# Usage:
+#   - Pre-requisite: Ensure that you have executed "source setup.sh"
 #   - Log into the AWS Console with your UDF cloud account
 #   - Execute: source quickstart.sh
 #   - The AWS Marketplace will automatically open in a new browser window. Subscribe to the AMI and wait for completion.
