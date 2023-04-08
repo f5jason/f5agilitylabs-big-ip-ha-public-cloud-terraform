a6ba413e2e43e0cd6ef4802073b0d71b730bf12b
Jason Chiu <f5jason@users.noreply.github.com>
2023-04-08 17:15:06 -0400

    Update cloudwatch.tf

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -12,9 +12,4 @@ resource "aws_cloudwatch_log_group" "log-group" {
 resource "aws_cloudwatch_log_stream" "log-stream" {
   name           = format("%s_%s_log_stream", var.prefix, var.aws_log_group)
   log_group_name = aws_cloudwatch_log_group.log-group.name
-
-  tags = {
-    Name  = format("%s_%s_log_stream", var.prefix, var.aws_log_group)
-    Owner = var.emailid
-  }
 }
