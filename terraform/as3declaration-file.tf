9342d2fb9d8224072f3dafe238cdb7d748c3eb4b
Carl B <44070777+CarlB-dev@users.noreply.github.com>
2023-04-12 17:16:22 -0400

    AS3 Cert Error
    Fixed a problem with the AS3 file for the certificate import.

--- terraform/as3declaration-file.tf
+++ terraform/as3declaration-file.tf
@@ -14,5 +14,5 @@ data "template_file" "as3certs" {
 resource "local_file" "as3certs_rendered" {
   depends_on = [null_resource.ecdsa_certs]
   content    = data.template_file.as3certs.rendered
-  filename   = "../ATC/AS3/lab1.3_as3_ecdsaCerts_autodiscovery"
+  filename   = "../ATC/AS3/lab1.3_as3_ecdsaCerts_autodiscovery.json"
 }
